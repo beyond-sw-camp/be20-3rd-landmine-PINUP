4aef0fc017d30aead066fa24264cf1e63d534f6c
waifuser <madobe7@naver.com>
2025-11-25 10:28:02 +0900

    feeds view create pinia store
    로그인 한 유저 정보 저장할 Pinia store 생성

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import '@/assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import {createPinia} from "pinia";
 
 createApp(App)
     .use(router)
     .use(ElementPlus)
+    .use(createPinia())
     .mount('#app')
